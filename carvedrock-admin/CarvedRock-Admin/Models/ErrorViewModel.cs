d5370dbeef2160cdd16b249defae5196e27002fa
os250082 <os250082@ncr.com>
2023-04-09 14:11:55 +0300

    Code refactoring

--- carvedrock-admin/CarvedRock-Admin/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CarvedRock_Admin.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
